c95643707fd88576afada547a4f1df70ea9a771c
Abdul Waheed <abwaheed.ahmad@gmail.com>
2023-01-07 00:22:30 +0500

    initial

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const About = () => {
+  return <div>All content related to About Us </div>;
+};
+
+export default About;
---
+++ pages/posts/users.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const users = () => {
+  return <div>All post by users will be shown here</div>;
+};
+
+export default users;
